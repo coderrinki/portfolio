1b6c07bbbc4890f0bc0bb70393c9fb16c6186c0b
coderrinki <128579571+coderrinki@users.noreply.github.com>
2024-02-24 09:59:56 +0530

    Add files via upload

---
+++ Rinki/script.js
@@ -0,0 +1,10 @@
+
+
+// for bedroom
+function imgSlider(anything){
+    document.querySelector('.image').src=anything;
+}
+function changeBgcolor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background=color;
+}
